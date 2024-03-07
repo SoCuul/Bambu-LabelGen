13c24cce3952cfda0b7230ff411b9ce6fc9c6009
SoCuul <63339559+SoCuul@users.noreply.github.com>
2024-03-06 20:09:19 -0800

    Remove accidental temp data

--- src/App.vue
+++ src/App.vue
@@ -43,34 +43,6 @@
         } as LabelDataType
     )
 
-    // Download formats
-    const items = [
-    {
-    icon: 'i-bx-link-external text-sm cursor-pointer',
-    title: 'Electronics',
-    subtitle: 'Mobile, Earbuds, TV',
-    avatarProps: { icon: 'i-bx-mobile-alt' },
-  },
-  {
-    icon: 'i-bx-link-external text-sm cursor-pointer',
-    title: 'Fashion',
-    subtitle: 'T-shirt, Jeans, Shoes',
-    avatarProps: { icon: 'i-bx-closet' },
-  },
-  {
-    icon: 'i-bx-link-external text-sm cursor-pointer',
-    title: 'Decor',
-    subtitle: 'Fine Art, Dining',
-    avatarProps: { icon: 'i-bx-home' },
-  },
-  {
-    icon: 'i-bx-link-external text-sm cursor-pointer',
-    title: 'Sports',
-    subtitle: 'Football, Cricket Kit',
-    avatarProps: { icon: 'i-bx-football' },
-  }
-    ]
-
     // URL State (decode on page load)
     onMounted(() => {
         // Decode data url parameter
